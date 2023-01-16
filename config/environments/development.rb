07ce1d8630af2c1ccd4d979fa3bce926120d86df
reikopi <rk02.stmi.1018@gmail.com>
2023-01-16 13:31:09 +0000

    [mokuhyo_tassei]サーバー接続

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "b9d75301b05b4c45b04ef737b49499b9.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
