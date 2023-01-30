ce8eccaf56a94a09021ec2648e2016fb491f4db1
reikopi <rk02.stmi.1018@gmail.com>
2023-01-30 11:59:13 +0000

    [mokuhyo_tassei]task_newのコントローラー作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,28 @@
 class TasksController < ApplicationController
-  
+
   def new
+    @task = Task.new
   end
-  
+
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
-  
+
   def show
   end
-  
+
   def index
   end
-  
+
+  private
+    def task_params
+      params.require(:task).permit(:deadline, :task, :comment)
+    end
+
+
 end
