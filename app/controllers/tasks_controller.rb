55e8bf6d2ba823ab1d64a4a2d23ff90cd3eeea93
reikopi <rk02.stmi.1018@gmail.com>
2023-02-26 04:17:51 +0000

    [mokuhyo_tassei]task_edit_編集開始_route編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   def show
   end
 
